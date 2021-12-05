20fdc73cedf7b29cfa97306a3232668b4f735a0d
amir <am8896626@gmail.com>
2021-12-05 23:39:05 +0330

    Blog Bugs fixed

--- src/component/Blog/Editor/Editor.js
+++ src/component/Blog/Editor/Editor.js
@@ -25,62 +25,32 @@ const modules = {
         matchVisual: false
     }
 };
-const Editor =()=> {
-    /*constructor(props) {
+class Editor extends React.Component {
+    constructor(props) {
         super(props)
         this.state = { text: '' } // You can also pass a Quill Delta here
         this.handleChange = this.handleChange.bind(this)
         this.Post = this.Post.bind(this)
-    }*/
-    const [value , SetValue]=useState('');
+    }
 
-    const handleChange = (event) => {
-        SetValue(event.target.value);
+    handleChange(value) {
+        this.setState({ text: value })
         console.log(value);
     }
-
-    const Post =async () =>
-    {
-            let item = {
-                value
-            };
-            fetch(url + "/auth/login",{
-                method:'POST',
-                mode: 'cors',
-                headers: {'Content-Type' : 'application/json'},
-                body:JSON.stringify(item)
-            })
-                .then(async response => {
-                    if(response.status === 201){
-                        response=await response.json();
-                        toast.success(response.message,{
-                            position:"top-right",
-                            closeOnClick:true
-                        });
-                    }
-                    else{
-                        toast.error("Failed to Post, try again later", {
-                            position: "top-right",
-                            closeOnClick: true
-                        });
-                        throw new Error('Failed to Post, try again later.\n' + response.statusText);
-                    }
-                })
-                .catch( err => console.log(err));
+    Post(value){
+        this.setState({ text: value })
+        //request to back
     }
-    return (
-        <>
+
+    render() {
+        return (
             <ReactQuill value={this.state.text}
                         theme="snow"
                         placeholder="Type here"
                         modules={modules}
                         onChange={this.handleChange} />
-            <Button variant="contained" color="primary" disableElevation
-                    style={{marginTop:"20px",marginBottom:"20px" , width:"auto", borderRadius:"15px"  }} onClick={Post} >
-                Post
-            </Button>
-        </>
-    );
 
+        )
+    }
 }
 export default Editor;
